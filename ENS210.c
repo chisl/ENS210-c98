352bf0ee0fe77a7b898ea336d0e0c52aaa22aa5a
chisl <info@chisl.io>
2017-12-27 18:12:35 +0100

    Initial commit

---
+++ ENS210.c
@@ -0,0 +1,9 @@
+/*
+ * name:        ENS210
+ * description: The ENS210 integrates one relative humidity sensor and one high-accuracy temperature sensor.
+ * manuf:       AMS
+ * version:     0.1
+ * url:         http://ams.com/eng/content/download/950231/2267959/483138
+ * date:        2017-09-03
+ */
+
